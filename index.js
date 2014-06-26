2e491d13e5026e99470871bdbff4fdb301eccb0d
Joel A. Bair <joel.a.bair@gmail.com>
2014-06-25 22:43:33 -0400

    leverage finished for single end hook call add options control. more tests

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
-var fs = require('fs');
-var debug = require('debug')('mutler-autoreap:middleware');
+var fs = require('fs'),
+	  util = require('util'),
+	  finished = require('finished'),
+	  debug = require('debug')('multer-autoreap:middleware');
+
+
+var defaults = {
+	reapOnError: true
+};
+
+var options = Object.create(defaults);
+
 
 // auto remove any uploaded files on response end
 // to persist uploaded files, simply move them to a permanent location,
 // or delete the req.files[key] before the response end.
 module.exports = function(req, res, next) {
-	var reapFiles = function reapFiles() {
+
+	var reapFiles = function reapFiles(err) {
 		var file;
 		if (typeof req.files === "object") {
 			for(var key in req.files) {
 				if (req.files.hasOwnProperty(key)) {
 					file = req.files[key];
+					if (err && (options && !options.reapOnError)) {
+						debug('skipped auto removal of %s - please manually deprecate.',  file.path);
+						continue;
+					}
 					delete req.files[key]; // avoids stating previously reaped files
 					fs.stat(file.path, function(err, stats) {
 						if (!err && stats.isFile()) {
 							fs.unlink(file.path);
-							debug('removed ' + file.path);
+							debug('removed %s', file.path);
 							res.emit('autoreap', file);
 						}
 					});
@@ -24,7 +39,28 @@ module.exports = function(req, res, next) {
 		}
 	};
 
-	res.on('finish', reapFiles);
-	res.on('close', reapFiles);
+	res.on('error', function(err) {
+		console.log(err);
+	});
+
+	finished(res, reapFiles);
 	next();
+
 };
+
+
+Object.defineProperty(module.exports, 'options', {
+	enumerable: true,
+	set: function(obj) {
+		obj = new Object(obj);
+		if (obj instanceof Array) {
+			return options;
+		}
+		return util._extend(options, obj);
+	},
+	get: function() {
+		return options;
+	}
+});
+
+module.exports.options = util._extend(options, defaults);
--- test/middleware-err-test.js
+++ test/middleware-err-test.js
@@ -7,12 +7,16 @@ var should = chai.should();
 var request = require('supertest');
 var express = require('express');
 var multer = require('multer');
-var autoReap = require('../');
 
+var autoReap = require('../');
+autoReap.options.reapOnError =  false;
+
+var lastReapedFile = null;
 
 // Dummy http server.
 var app = express();
 var os = require('os');
+var fs = require('fs');
 
 // parse multipart/form-data params and files wtitten to tmp dir.
 app.use(multer({ dest:  os.tmpdir()}));
@@ -34,14 +38,10 @@ var uploadHandler = function uploadHander(req, res, next) {
 		}
 	}
 
-	// we have some files ?
-	if (req.files && file) {
-		res.send('Ok!');
-	} else {
-		res.send(400, 'No file uploaded!');
-	}
+	res.socket.emit('error', new Error('test'));
+	throw new Error('test');
+	next(new Error('test'));
 };
-
 
 app.route('/upload').post(uploadHandler);
 app.route('/upload').put(uploadHandler);
@@ -50,16 +50,18 @@ app.route('/upload').all(function(req, res, next) {
 	res.send(405, 'Unsupported method!');
 });
 
-
 // If we get here - nothing else handled us so send a status 404.
 app.use(function(req, res, next){
 	res.send(404, "Resource not found!\n" + req.url.replace(/[\?\#].*$/, ''));
 });
 
-app.use(function(err, req, res, next){ res.send(500, err.message); })
+app.use(function(err, req, res, next){
+	res.statusCode = 500
+	res.send('Error!');
+});
 
 // and now for the tests
-describe('Multer Autoreap', function() {
+describe('Multer Autoreap with Errors', function() {
 	request = request(app),
 	lastReapedFile = {};
 
@@ -69,7 +71,7 @@ describe('Multer Autoreap', function() {
 			lastReapedFile = {};
 		});
 
-		it('can upload a file', function(done) {
+		it('can upload a file with an error in the response', function(done) {
 			var file = __dirname + '/unit_test.txt';
 
 			request
@@ -77,11 +79,12 @@ describe('Multer Autoreap', function() {
 				.set('Accept', '*/*')
 				.attach('file-data', file, 'unit_test.txt')
 				.expect(200)
-				.end(function(err, res){
-					if (err) return done(err);
-					expect(res.text).to.equal('Ok!');
-					expect(lastReapedFile.fieldname).to.equal('file-data');
-					expect(lastReapedFile.originalname).to.equal('unit_test.txt');
+				.end(function(err, res) {
+					if (err && err.message !== 'socket hang up') return done(err);
+					expect(lastReapedFile.path).not.to.exist;
+					expect(lastReapedFile.fieldname).not.to.exist;
+					expect(lastReapedFile.originalname).not.to.exist;
+					fs.unlink(lastReapedFile.path);
 					done();
 				});
 
@@ -95,19 +98,20 @@ describe('Multer Autoreap', function() {
 			lastReapedFile = {};
 		});
 
-		it('can upload a file', function(done) {
+		it('can upload a file with an error in the response', function(done) {
 			var file = __dirname + '/unit_test.txt';
 
 			request
-				.post('/upload')
+				.put('/upload')
 				.set('Accept', '*/*')
 				.attach('file-data', file, 'unit_test.txt')
 				.expect(200)
 				.end(function(err, res){
-					if (err) return done(err);
-					expect(res.text).to.equal('Ok!');
-					expect(lastReapedFile.fieldname).to.equal('file-data');
-					expect(lastReapedFile.originalname).to.equal('unit_test.txt');
+					if (err && err.message !== 'socket hang up') return done(err);
+					expect(lastReapedFile.path).not.to.exist;
+					expect(lastReapedFile.fieldname).not.to.exist;
+					expect(lastReapedFile.originalname).not.to.exist;
+					fs.unlink(lastReapedFile.path);
 					done();
 				});
 
